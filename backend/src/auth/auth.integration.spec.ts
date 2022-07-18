437c8e3dffa46aea2314e14fa7b1ad6db7fbcef1
ckm96 <chriskmoore96@gmail.com>
2022-07-18 11:50:11 +0100

    Add validation pipe

--- backend/src/auth/auth.integration.spec.ts
+++ backend/src/auth/auth.integration.spec.ts
@@ -1,7 +1,7 @@
 import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { AuthModule } from './auth.module';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from '../users/users.entity';
 
@@ -24,31 +24,44 @@ describe('Auth', () => {
     }).compile();
 
     app = moduleRef.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe());
     await app.init();
   });
 
   describe('register', () => {
-    it('registers users', () =>
-      request(app.getHttpServer())
+    it('registers users', () => {
+      return request(app.getHttpServer())
         .post('/auth/register')
-        .send({ email: 'email', password: 'password' })
-        .expect(201));
+        .send({ email: 'email@email.com', password: 'password' })
+        .expect(201);
+    });
 
-    it('returns a 400 if the request body is incorrectly formatted', () => {});
+    it('returns a 400 if the request body is incorrectly formatted', () => {
+      return request(app.getHttpServer())
+        .post('/auth/register')
+        .send({ email: 'email', password: '' })
+        .expect({
+          statusCode: 400,
+          message: ['email must be an email', 'password should not be empty'],
+          error: 'Bad Request'
+        });
+    });
   });
 
   describe('login', () => {
-    it('logs in existing users', () =>
-      request(app.getHttpServer())
+    it('logs in existing users', () => {
+      return request(app.getHttpServer())
         .post('/auth/login')
-        .send({ email: 'email', password: 'password' })
-        .expect(201));
+        .send({ email: 'email@email.com', password: 'password' })
+        .expect(201);
+    });
 
-    it('returns a 401 if logging in with invalid credentials', () =>
-      request(app.getHttpServer())
+    it('returns a 401 if logging in with invalid credentials', () => {
+      return request(app.getHttpServer())
         .post('/auth/login')
-        .send({ email: 'email', password: 'wrongPassword' })
-        .expect(401));
+        .send({ email: 'email@email.com', password: 'wrongPassword' })
+        .expect(401);
+    });
   });
 
   afterAll(async () => {
--- backend/src/users/user.dto.ts
+++ backend/src/users/user.dto.ts
@@ -1,4 +1,9 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
 export class UserDto {
+  @IsEmail()
   email: string;
+  
+  @IsNotEmpty()
   password: string;
 }
