2dd9e9498ab3015e3893bb1f46b9fbdf2ecd9f87
ckm96 <chriskmoore96@gmail.com>
2022-07-19 17:06:55 +0100

    Add JWT auth guards to project endpoints

--- backend/src/projects/projects.controller.ts
+++ backend/src/projects/projects.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Post, Put } from '@nestjs/common';
+import { Body, Controller, Get, Post, Put, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { InsertProjectDto, UpdateProjectDto } from './project.dto';
 import { ProjectsService } from './projects.service';
 
@@ -6,16 +7,19 @@ import { ProjectsService } from './projects.service';
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async insert(@Body() project: InsertProjectDto) {
     await this.projectsService.insertProject(project);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put()
   async update(@Body() project: UpdateProjectDto) {
     await this.projectsService.updateProject(project);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async getAll() {
     return await this.projectsService.getAll();
--- backend/src/projects/projects.module.ts
+++ backend/src/projects/projects.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsService } from './projects.service';
 import { Projects } from './projects.entity';
 import { ProjectsController } from './projects.controller';
+import { JwtStrategy } from '../auth/jwt.strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Projects])],
-  providers: [ProjectsService],
+  providers: [ProjectsService, JwtStrategy],
   exports: [ProjectsService],
   controllers: [ProjectsController],
 })
