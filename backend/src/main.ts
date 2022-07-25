9953abea21c70dac6b3103117f3b80fabc79967e
ckm96 <chriskmoore96@gmail.com>
2022-07-25 15:35:59 +0100

    Enable CORS in the backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,12 @@
+require('dotenv').config();
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ origin: process.env.FRONTEND_ORIGIN });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
