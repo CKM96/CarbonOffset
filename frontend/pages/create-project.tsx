1f421143b964c55f28cf0f290ebd7398473f0688
ckm96 <chriskmoore96@gmail.com>
2022-07-26 12:40:47 +0100

    Use access token when creating projects

--- frontend/pages/create-project.tsx
+++ frontend/pages/create-project.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useState } from 'react';
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
+import cookie from 'cookie';
 
 const Form = styled.form`
   display: flex;
@@ -19,28 +20,30 @@ const SubmitButton = styled.input`
 `;
 
 function CreateProject() {
-  const [name, setName] = useState<string>('');
-  const [description, setDescription] = useState<string>('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
 
   const router = useRouter();
 
   const onSubmit = async (event: FormEvent) => {
     event.preventDefault();
+    const token = cookie.parse(document.cookie)?.accessToken;
     const res = await fetch('http://localhost:3001/projects', {
       method: 'post',
       headers: {
-        Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImVtYWlsQGVtYWlsd3Nhc3cxMjM0LmNvbSIsInN1YiI6IjE2MTIxODIzLWRlZDMtNDMzYy1iMzFjLTZmNWFiZmU1MThmMSIsImlhdCI6MTY1ODc1OTQ2NSwiZXhwIjoxNjU4NzYzMDY1fQ.nNl_j20bfluTwFwYRB5TAF3gBRB-Vi5l7h4r5VNzjQA',
+        Authorization: `Bearer ${token}`,
         Origin: 'localhost:3000',
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         name,
-        description
+        description,
       }),
     });
     if (res.ok) {
       router.push('/home');
+    } else {
+      router.push('/login');
     }
   };
 
